4cd1bbb7dc232ffa01aa17226cf12d35a85e39a4
anthonychibuike <anthonychibuike246@gmail.com>
2020-04-30 00:49:43 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'darshboard.dart';
+
+void main(){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dashboard Demo',
+      theme: ThemeData(
+        primaryColor: new Color(0xff622F74),
+      ),
+      home: Dashboard(),
+    );
+
+  }
+}
